2a3de4611c91146dffafe05a4912976c61ba2ef6
Henrique Galinha <hgalinha1999@hotmail.com>
2026-01-20 16:51:52 +0000

    .

---
+++ vasto/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://vastoimperio.pt', // o teu domínio
+  generateRobotsTxt: true, // cria também o robots.txt
+  sitemapSize: 5000, // se tiveres muitas páginas
+};

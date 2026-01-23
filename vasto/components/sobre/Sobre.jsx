adebc54e921d3635bc44961496b3bf97181e6ca3
Henrique Galinha <hgalinha1999@hotmail.com>
2026-01-23 09:15:21 +0000

    .

--- vasto/components/sobre/Sobre.jsx
+++ vasto/components/sobre/Sobre.jsx
@@ -15,12 +15,12 @@ function Sobre() {
     <div className="text-medium border-bm-auto border-(--horizontal-line) mb-20">
       <h1 className="bg-(--gray) p-5 text-center title font-black text-[1.8em] md:text-[2.5em]">{item.title}</h1>
       <div className="flex py-15 flex-col gap-5 m-auto">
-        <div className="flex flex-col md:flex-row max-w-250 m-auto">
+        <div className="flex flex-col text-justify md:flex-row max-w-250 m-auto">
             <div className="max-w-200 m-auto px-5">
-              <p className="text-(--orange) font-black text-[1.2em]">{item.p1t}</p>
+              <p className="text-(--orange) text-left font-black text-[1.2em]">{item.p1t}</p>
               <p>{item.p1}</p>
               <br />
-              <p className="text-(--blue) font-black text-[1.2em]">{item.p2t}</p>
+              <p className="text-(--blue) text-left font-black text-[1.2em]">{item.p2t}</p>
               <p>{item.p2}</p>
             </div>
             <Image
@@ -31,7 +31,7 @@ function Sobre() {
               className="my-5 mx-auto drop-shadow-[0_8px_8px_rgba(0,0,0,0.35)] w-full max-w-110"
             />
         </div>
-        <div className="flex gap-5 text-center items-center justify-center text-[5em] md:text-[6em] lg:text-[7em] xl:text-[8em] 2xl:text-[9em] font-black bg-(--gray)">
+        <div className="flex gap-5 text-center items-center justify-center text-[5em] md:text-[10em] lg:text-[13em] font-black bg-(--gray)">
           
           <p className=" drop-shadow-[0_8px_8px_rgba(0,0,0,0.35)] text-(--orange)">
             {item.vinte}
@@ -46,11 +46,11 @@ function Sobre() {
                 </div>
                 <p className=" drop-shadow-[0_8px_8px_rgba(0,0,0,0.35)] text-(--blue)">{item.anos}</p> */}
         </div>
-        <div className="max-w-250 py-10 m-auto px-5">
-          <p className="text-(--orange) font-black text-[1.2em]">{item.p3t}</p>
+        <div className="max-w-250 py-10 text-justify m-auto px-5">
+          <p className="text-(--orange) font-black text-left text-[1.2em]">{item.p3t}</p>
           <p>{item.p3}</p>
           <br />
-          <p className="text-(--blue) font-black text-[1.2em]">{item.p4t}</p>
+          <p className="text-(--blue) font-black text-left text-[1.2em]">{item.p4t}</p>
           <p>{item.p4}</p>
         </div>
       </div>
